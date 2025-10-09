ddc08a7f1fcabc93d1e4154341b606a692107c45
timothyleejin <timothyleejin@gmail.com>
2025-10-09 16:51:41 +0800

    Edit EditCommand feature to allow users to edit role

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,6 +43,8 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Role: ")
+                .append(person.getRole())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
