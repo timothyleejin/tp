4dd5108280372946a50bbaf4a7a71c84e48ee92e
timothyleejin <timothyleejin@gmail.com>
2025-10-16 01:21:46 +0800

    Edit HelpCommand to include more information on commands

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -13,7 +13,17 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "Commands that you can use:\n" +
+            "list: list all contacts\n" +
+            "add: add a contact\n" +
+            "delete: delete a contact\n" +
+            "clear: deletes all contacts\n" +
+            "find: finds a contact using name\n" +
+            "filter: filters contacts by events/role\n" +
+            "edit: edit a contact's information\n" +
+            "exit: exit the address book\n" +
+            "For more information, visit\n" +
+            "https://ay2526s1-cs2103t-f13-2.github.io/tp/UserGuide.html";
 
     @Override
     public CommandResult execute(Model model) {
