3fcd90a490493a1e67ddc2912aed5ff5f3c734d3
isaaclim01 <e1366771@u.nus.edu>
2025-10-30 01:11:17 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/person/FilterPredicate.java
+++ src/main/java/seedu/address/model/person/FilterPredicate.java
@@ -2,8 +2,6 @@ package seedu.address.model.person;
 
 import java.util.function.Predicate;
 
-import seedu.address.commons.util.StringUtil;
-
 /**
  * Tests that a {@code Person}'s fields matches all the given keywords
  */
@@ -40,32 +38,33 @@ public class FilterPredicate implements Predicate<Person> {
 
         boolean checkName = filterParams.getNames().isEmpty()
                 || filterParams.getNames().stream().anyMatch(
-                    keyword -> person.getName().fullName.toLowerCase().contains(keyword.fullName.toLowerCase()));
+                        keyword -> person.getName().fullName.toLowerCase().contains(keyword.fullName.toLowerCase()));
 
         boolean checkPhone = filterParams.getPhones().isEmpty()
                 || filterParams.getPhones().stream().anyMatch(
-                    phone -> person.getPhone().value.toLowerCase().contains(phone.toLowerCase()));
+                        phone -> person.getPhone().value.toLowerCase().contains(phone.toLowerCase()));
 
         boolean checkEmail = filterParams.getEmails().isEmpty()
                 || filterParams.getEmails().stream().anyMatch(
-                    email -> person.getEmail().value.toLowerCase().contains(email.toLowerCase()));
+                        email -> person.getEmail().value.toLowerCase().contains(email.toLowerCase()));
 
         boolean checkTelegram = filterParams.getTelegrams().isEmpty()
                 || filterParams.getTelegrams().stream().anyMatch(
-                    telegram -> person.getTelegram().value.toLowerCase().contains(telegram.toLowerCase()));
+                        telegram -> person.getTelegram().value.toLowerCase().contains(telegram.toLowerCase()));
 
         boolean checkRole = filterParams.getRoles().isEmpty()
                 || filterParams.getRoles().stream().anyMatch(
-                    role -> person.getRole().value.toLowerCase().contains(role.value.toLowerCase()));
+                        role -> person.getRole().value.toLowerCase().contains(role.value.toLowerCase()));
 
         boolean checkEvent = filterParams.getEvents().isEmpty()
                 || filterParams.getEvents().stream().anyMatch(
-                    event -> person.getEvent().value.toLowerCase().contains(event.value.toLowerCase()));
+                        event -> person.getEvent().value.toLowerCase().contains(event.value.toLowerCase()));
 
         boolean checkSkill = filterParams.getSkills().isEmpty()
                 || filterParams.getSkills().stream().anyMatch(
-                        skill -> person.getSkills().stream().anyMatch(personSkill ->
-                        personSkill.skillName.toLowerCase().contains(skill.skillName.toLowerCase())));
+                        skill -> person.getSkills().stream().anyMatch(
+                                personSkill -> personSkill.skillName.toLowerCase()
+                                        .contains(skill.skillName.toLowerCase())));
 
         boolean matchFilters = checkName && checkPhone && checkEmail && checkTelegram
                 && checkRole && checkEvent && checkSkill;
